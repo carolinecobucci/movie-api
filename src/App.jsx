7e9371cd54a23c80f425524ba611a92a99159c60
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2024-01-09 14:35:42 -0300

    settings update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <nav>
-        <h2>
-          <Link to="/">MoviesLib</Link>
-        </h2>
-        <Link to="/movie/1">Movie</Link>
-        <Link to="/search">Search</Link>
-      </nav>
+    <div className="app-container">
+      <NavBar />
       <Outlet />
     </div>
   );
